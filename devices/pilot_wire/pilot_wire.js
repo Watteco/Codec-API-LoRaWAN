b1ddacadebde402f757738c81ff1582a46c0391f
Jean-Baptiste Noblot <jbnoblot@consignity.fr>
2023-11-14 09:59:52 +0100

    suppression d'une variable undefined et inutile

--- devices/pilot_wire/pilot_wire.js
+++ devices/pilot_wire/pilot_wire.js
@@ -3,6 +3,6 @@ let watteco = require("../decode_uplink")
 let batch_param=[]
 let endpointCorresponder={}
 function decodeUplink(input) {
-    return result = watteco.watteco_decodeUplink(input, batch_param, endpointCorresponder);
+    return watteco.watteco_decodeUplink(input, batch_param, endpointCorresponder);
 }
 exports.decodeUplink = decodeUplink;
